6a48c23ca43d5be7758a9f61bf2d26917a9824c0
KhanAleXX <zhumatayev.alikhan@gmail.com>
2024-10-15 16:27:55 -0400

    Part 6 progress

---
+++ Part6/include/console.h
@@ -0,0 +1,9 @@
+#ifndef MYOS_INCLUDE_CONSOLE_H
+#define MYOS_INCLUDE_CONSOLE_H
+
+void clear_terminal();
+void print_character(char c);
+void print_string(char* str);
+void print_line(char* str);
+
+#endif
--- Part6/kernel.c
+++ Part6/kernel.c
@@ -1,13 +1,10 @@
+#include "include/console.h"
+#include "console/console.c"
+
 void main(void) {
 	char* const VGA_BUFFER = (char*) 0xb8000;
 
 	char* str = "Hello World!";
 
-	for (int i=0; str[i] !='\0'; i++) {
-
-		VGA_BUFFER[i*2] = str[i];
-		VGA_BUFFER[i*2 + 1] = 0x04;
-
-	}
-//	while (1) {}
+	print_line(str);
 }
